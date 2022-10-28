0e3eaef592429b9dca724dd3546b49ef83717470
Lamiaa Gebril <lamiaamonaem6@gmail.com>
2022-10-28 17:24:21 +0200

    first commit

---
+++ middleware.js
@@ -0,0 +1,34 @@
+import jwt from "jsonwebtoken";
+import Author from "./models/author.js";
+
+// Check at each request for a certain pages if the user is logged in
+const requireAuth = (req, res, next) => {
+  const token = req.cookies.jwt;
+  if (token) {
+    jwt.verify(token, process.env.JWT_SECRET_KEY, (err, decodedToken) => {
+      if (err) res.redirect("/login");
+      else next();
+    });
+  } else res.redirect("/login");
+};
+
+
+// Check at each request if the user is logged in
+const checkCurrentAuthor = async (req, res, next) => {
+  const token = req.cookies.jwt;
+  if (token) {
+    const result = await jwt.verify(token, process.env.JWT_SECRET_KEY, async (err, decodedToken) => {
+      if(err) return { errors: err.message }
+      return decodedToken
+    });
+    if (result.errors) res.locals.author = null;
+    else {
+      let author = await Author.findById(result.id);
+      res.locals.author = author?.name || null;
+    }
+  } else res.locals.author = null;
+  next();
+
+};
+
+export { requireAuth, checkCurrentAuthor };
